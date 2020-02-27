7ea8c27dfc072ffba294cd34bbb3b91b59d6640f
Tobi <tbravmann@bavariantools.org>
2020-02-27 10:17:59 +0100

    Initial commit

---
+++ src/webparts/userMgmt/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IUserMgmtWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'UserMgmtWebPartStrings' {
+  const strings: IUserMgmtWebPartStrings;
+  export = strings;
+}
